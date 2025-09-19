9708d1624453ab507fc3b6286161b203d4aa4655
amit <dev.rahuljangra@gmail.com>
2025-09-19 20:12:43 +0530

    -Initial commit

---
+++ app/src/main/java/com/recipeapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.recipeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.recipeapp.ui.theme.RecipeAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RecipeAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Text(text = "Rahul Jangra",
+                        modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
